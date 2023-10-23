ec8d4a5ec4d8be8e8902924e11133cf69ad758b8
Victor Hristov <Victor.h7@live.com>
2023-10-23 14:22:15 +0200

    Initial Commit

---
+++ wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by Victor Hristov on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
